debba6570c7050c8c653e72fedea19b8b9595f2a
刘智勇 <690284618@qq.com>
2017-09-30 16:43:29 +0800

    上传商品图片(未完)

--- www/views/product/product-add.controller.js
+++ www/views/product/product-add.controller.js
@@ -1,9 +1,12 @@
 (function () {
   'use strict';
-  angular.module('starter.controllers').controller('ProductAddCtrl',['$scope','CategoryService',function ($scope,CategoryService) {
+  angular.module('starter.controllers').controller('ProductAddCtrl',['$scope','CategoryService','$cordovaBarcodeScanner','$cordovaCamera','$cordovaImagePicker','$ionicActionSheet',
+    function ($scope,CategoryService,$cordovaBarcodeScanner,$cordovaCamera,$cordovaImagePicker,$ionicActionSheet) {
     $scope.product={
       ID:0,
       Name:'',
+      Barcode:'',
+      //Images:['/views/product/img/1.jpg','/views/product/img/2.jpg','/views/product/img/3.jpg'],
       CategoryID:CategoryService.activeCategory.ID,
       Category:{
         ID:CategoryService.activeCategory.ID,
@@ -16,5 +19,13 @@
       $scope.product.Category.ID=CategoryService.activeCategory.ID;
       $scope.product.Category.Name=CategoryService.activeCategory.Name;
     });
+      $scope.scanBarcode = function () {
+        $cordovaBarcodeScanner.scan().then(function (data) {
+          $scope.product.Barcode = data.text;
+        },
+        function (error) {
+
+        });
+      }
   }]);
 })();
